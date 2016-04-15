bf6594a2ded1ad1c8295d2862fb1e8598cf616d6
Turan Karatuğ <tkaratug@hotmail.com.tr>
2016-04-15 16:52:18 +0300

    Update Example.php

--- Example.php
+++ Example.php
@@ -10,25 +10,25 @@ if($_POST) {
 
 	echo $form->open('form_1', ['action' => 'Example.php', 'method' => 'post']);
 
-	echo $form->label('test', 'Test Giriniz', ['style' => 'display: block;']);
-	echo $form->input('firstname', ['placeholder' => 'Ad, Soyad']) . '<br><br>';
+	echo $form->label('test', 'Full Name', ['style' => 'display: block;']);
+	echo $form->input('firstname', ['placeholder' => 'Full Name']) . '<br><br>';
 
 	echo $form->hidden('user_id', 15);
 
-	echo $form->textarea('aciklama', 'test', ['class' => 'eghe']) . '<br><br>';
+	echo $form->textarea('description', 'test', ['class' => 'foo']) . '<br><br>';
 
 	echo $form->password('password') . '<br><br>';
 
-	echo $form->custom('email', 'email', ['class' => 'test']) . '<br><br>';
+	echo $form->custom('email', 'email', ['class' => 'bar']) . '<br><br>';
 
-	echo $form->select('adaylar', ['Hasan' => 1, 'Fehmi' => 2], 2, ['class' => 'aga']) . '<br><br>';
+	echo $form->select('adaylar', ['Person A' => 1, 'Person B' => 2], 2, ['class' => 'foo']) . '<br><br>';
 
-	echo $form->multiSelect('gruplar', ['Meyve' => 1, 'Sebze' => 2, 'Tatli' => 3, 'Tahil' => 4], 2, ['class' => 'aga']) . '<br><br>';
+	echo $form->multiSelect('groups', ['Group A' => 1, 'Group B' => 2, 'Group C' => 3, 'Group D' => 4], 2, ['class' => 'bar']) . '<br><br>';
 
-	echo $form->checkbox('gerekli', 1, false, ['style' => 'width: 15px;']) . '<br><br>';
+	echo $form->checkbox('Accept', 1, false, ['style' => 'width: 15px;']) . '<br><br>';
 
-	echo $form->radio('radyo', 1, true, ['style' => 'width: 15px;']);
-	echo $form->radio('radyo', 2, false, ['style' => 'width: 15px;']) . '<br><br>';
+	echo $form->radio('radio_1', 1, true, ['style' => 'width: 15px;']);
+	echo $form->radio('radio_2', 2, false, ['style' => 'width: 15px;']) . '<br><br>';
 
 	echo $form->file('image', true) . '<br><br>';
 
